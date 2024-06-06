e889121cdd81fa0bf2b7ffdb28f62eb8befbdd42
Lu Yuanbo <Lucas.Lu@grapecity.com>
2024-06-06 15:21:39 +0900

    ioc注入

---
+++ LucasHelper/Framework/ServiceAttribute.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace LucasHelper.Framework
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ServiceAttribute : Attribute
+    {
+        public ServiceLifetime Lifetime { get; set; } = ServiceLifetime.Transient;
+
+    }
+}
